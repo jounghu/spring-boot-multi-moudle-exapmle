6cbba7bdae37d27b0f36c052f97af10119f57ff1
unknown <hujiansong@dobesp.corp>
2018-04-19 12:48:48 +0800

    [Add] init

---
+++ core/src/main/java/com/dobest/bfrdadcollection/BfrdAdcollectionApplication.java
@@ -0,0 +1,12 @@
+package com.dobest.bfrdadcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfrdAdcollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BfrdAdcollectionApplication.class, args);
+    }
+}
---
+++ core/src/test/java/com/dobest/bfrdadcollection/BfrdAdcollectionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dobest.bfrdadcollection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BfrdAdcollectionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
